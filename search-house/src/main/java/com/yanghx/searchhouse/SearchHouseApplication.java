8e60dd44d62cc056ad15c67d4241d4e8ba894a3d
yanghx <yanghx_mail@163.com>
2019-05-16 14:14:27 +0800

    firse commit

---
+++ search-house/src/main/java/com/yanghx/searchhouse/SearchHouseApplication.java
@@ -0,0 +1,13 @@
+package com.yanghx.searchhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchHouseApplication.class, args);
+    }
+
+}
